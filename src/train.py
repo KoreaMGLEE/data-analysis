dc71de82997227ab7ffed1c64c4cc3a4b5ebf786
mingyu lee <mingyulee@mingyus-MacBook-Air.local>
2025-12-18 13:42:48 +0900

    change default model

--- src/train.py
+++ src/train.py
@@ -235,7 +235,7 @@ def main():
     import argparse
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name", type=str, default="EleutherAI/pythia-30m")
+    parser.add_argument("--model_name", type=str, default="EleutherAI/pythia-14m")
     parser.add_argument("--output_dir", type=str, default="./checkpoints/pythia-30m-mnli")
     parser.add_argument("--num_epochs", type=int, default=3)
     parser.add_argument("--batch_size", type=int, default=8)
